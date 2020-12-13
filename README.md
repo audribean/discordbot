e83bc341aab7fbeb9f791d8d071801ceffcc0ea2
Audri Saulters <2007848@apps.nsd.org>
2020-12-13 01:22:44 -0800

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Audri's Discord Bot</h1>
+<img src="/Users/Audri/Desktop/untitled folder/files/Preview.png"
+     alt="Preview of Audri's bot. It displays a message pinned by the bot displaying custom commands. It reads: Welcome to Audri's Server! Here are some commands: !pin followed by a message to
+     quickly pin something. !clean followed by 'all' or an integer between 1 and 98 to delete
+     that many previous messages. !roles to assign yourself roles. !nick followed by a name to
+     give yourself a new nickname. !invite to create an invite link to the server. !new to create a new role"/>
+---
+<h2>About</h2>
+<p>This bot automates several features within the messaging platform<a href="discord.com"> Discord </a>
+using discord.js commando, a node.js module for interacting with Discord APIs. These features can be called
+by users by typing a key phrase, such as "!pin" as seen above. This bot not only saves me (and other admins who use it) time, but makes the platform more accessible to people who are new to Discord. For example, admins won't have to manually assign roles to every new member in their server or tediously delete every spam message. Members can assign roles to themselves by clicking emojis, and admins can delete many messages at once.</p>
+
---
+++ index.js
@@ -0,0 +1,257 @@
+const Discord = require("discord.js");
+require("dotenv-flow").config();
+const bot = new Discord.Client();
+const prefix = "!";
+const chanID = "784928448811892777";
+const guildID = "784928448367427615";
+const roles = {
+    '🐷': "787212472666685460",
+    '🐸': "787212690388418570"
+}
+
+// EFFECTS: outputs that the bot is logged in and pins the instruction message to the general channel
+function onReady(){
+    console.log('Logged in as ' + bot.user.tag)
+
+    const channel = bot.channels.cache.get(chanID)
+    const embed = new Discord.MessageEmbed
+    embed.setTitle("Welcome to Audri's server!")
+    embed.setDescription("Here are some commands:\n"
+            + "**" + prefix + "pin** followed by a message to quickly pin something\n"
+            + "**" + prefix + "clean** followed by 'all' or an integer between 0 and 98 "
+                + "to delete a number of previous messages in the channel\n"
+            + "**" + prefix + "roles** to assign yourself roles\n"
+            + "**" + prefix + "nick** followed by a name to give yourself a new nickname!\n"
+            + "**" + prefix + "invite** to create an invite link to the server\n"
+            + "**" + prefix + "new** to create a new role")
+    embed.setColor([157, 193, 131])
+    channel.send(embed)
+        .then(msg => msg.pin())
+        .catch(console.error)
+}
+
+// EFFECTS: handles calls to !pin, !clean, and does nothing otherwise
+function onMessage(message) {
+    if (message.author.bot || message.author == bot.user) {
+        //console.log('Ignored bot message')
+        return
+        }
+    if (message.content.startsWith(prefix + "pin")) {
+        pinIt(message)
+    } else if (message.content.startsWith(prefix + "clean")) {
+        cleanChan(message)
+    } else if (message.content.startsWith(prefix + "roles")) {
+        roleReact(message)
+    } else if (message.content.startsWith(prefix + "nick")){
+        newNick(message)
+    } else if (message.content.startsWith(prefix + "invite")){
+        createInvite(message)
+    } else if (message.content.startsWith(prefix + "new")) {
+        createRole(message)
+    }
+}
+
+// REQUIRES: (string) message to be written after the !pin
+// EFFECTS: sends a message with desired content and pins it, deleting the user's !pin call
+function pinIt(message) {
+    message.channel.send("@" + message.author.username + " wrote:\n " + message.content.substr(5))
+        .then(msg => msg.pin())
+        .then(console.log)
+        .catch(console.error)
+    message.delete()
+}
+
+// REQUIRES: an integer i between 1-98 inclusive to follow !clean
+// EFFECTS: if user confirms they want to clean the channel, i messages are deleted as well as the last "!clean" call
+//          if user cancels, message is sent reaffirming cancellation
+//          if user does nothing after 30 seconds, message is sent reaffirming that the user did nothing,
+//          and this message is deleted after an additional 10 seconds 
+function cleanChan(message) {
+    // counts how many messages the user wants to delete
+    let num
+    if (message.content.substr(7) === "all") {
+        num = 98
+    } else {
+        num = parseInt(message.content.substr(6))
+    }
+
+    // confirmation message
+    message.reply("Are you sure you want to delete the last " + num + " messages? Click 👍 to continue and 👎 to cancel.")
+    message.react('👍')
+    message.react('👎')
+
+    // sees if the user reacted with one of two possible emoji reacts
+    const filter = (reaction, user) => {
+        return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+    }
+
+    message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+        .then(collected => {
+            const reaction = collected.first()
+            // deletes bot messages, call to clean, and desired number of messages
+            if (reaction.emoji.name === '👍') {
+                if (message.channel.type = "text") {
+                    message.channel.bulkDelete(num + 2)
+                        .then(messages => console.log("Deleted " + messages.size + " messages"))
+                        .catch(console.error)
+                }
+            }   else {
+                    // deletes the bot message and call to clean
+                    message.channel.bulkDelete(2)
+                    message.channel.send(message.author.username + " cancelled cleaning the channel")
+                }
+            })
+            .catch(() => {
+                // handles case where user doesn't react within 30 seconds
+                message.reply("You didn't react within 30 seconds of calling !clean")
+                .then(msg => {
+                    setTimeout(() => {
+                        msg.delete()
+                        message.delete()
+                    }, 10000) 
+            })})
+}
+
+// EFFECTS: If user reacts to pinned message with pig or frog emoji AND they don't have that
+//          role already, they receive that role. If they react twice to a role they have,
+//          that role will be removed.
+function roleReact(message) {
+    const roleEmbed = new Discord.MessageEmbed
+    roleEmbed.setTitle("Roles")
+    roleEmbed.setDescription("🐷: Peppa Pig\n"
+        + "🐸: Kermit the Frog")
+    roleEmbed.setColor([198, 181, 217])
+    
+    message.channel.send(roleEmbed).then(async msg => {
+        msg.react('🐷')
+        msg.react('🐸')
+        const emojis = Object.keys(roles)
+        for (const emoji of emojis) {
+            await msg.react(emoji)
+        }
+        const collector = msg.createReactionCollector(({emoji}) => emojis.includes(emoji.name))
+        // Handles role assignment once emoji is clicked
+        collector.on('collect', ({emoji, message}, user) => {
+            message.guild.members.fetch(user).then(member => {
+                // Case where user already has role
+                if (member.roles.cache.has(roles[emoji.name])) {
+                    message.reply("You already have that role!")
+                } else {
+                    // Case where user gets new role
+                    member.roles.add(roles[emoji.name])
+                    console.log(user.tag + " reacted with " + emoji.name + " role")
+                    message.guild.roles.fetch(roles[emoji.name])
+                        .then(role => message.reply("Congrats! You are now " + role.name))
+                }
+            })
+        })
+        // Note: Seems like there are some widespread issues with the "remove" event,
+        //       so this function might not work until the next update
+        collector.on('remove', ({emoji, message}, user) => {
+            message.guild.members.fetch(user).then(member => {
+                console.log(user.username + " removed a role")
+                if (member.roles.cache.has(roles[emoji.name])) {
+                    console.log(user.username + " has the role they want to remove")
+                    member.roles.remove(roles[emoji.name])
+                    message.guild.roles.fetch(roles[emoji.name])
+                        .then(role => message.reply("You are no longer " + role.name))
+                }
+            })
+        })
+    })
+}
+
+// REQUIRES: non-null string to follow !nick
+// EFFECTS: replaces the user's nickname with desired name
+//          if the user is not the owner/higher in hierarchy
+function newNick(message) {
+    let newName = message.content.substr(6).toString()
+    message.member.setNickname(newName)
+    .catch(console.error)
+    console.log(newName)
+    message.reply("love the new look, " + message.member.nickname)
+}
+
+// EFFECTS: Creates a temporary invite when reacted with '😄', and a permanent invite with '🤩'
+function createInvite(message) {
+    const server = bot.guilds.fetch(guildID)
+    const channel = bot.channels.cache.get(chanID)
+    
+    // handles the emoji reactions for invite customization
+    message.reply("React with '😄' for a temporary invite, and '🤩' for a permanent invite")
+        .then(msg =>
+            {
+                msg.react('😄')
+                msg.react('🤩')
+                const filter = (reaction, user) => {
+                    return ['😄','🤩'].includes(reaction.emoji.name) && user.id === message.author.id;
+                }
+                // handles emoji reactions
+                msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+                    .then(collected => {
+                        const reaction = collected.first()
+                        if (reaction.emoji.name === '😄') {
+                            channel.createInvite({maxUses: 1, maxAge: 60000, guild: server, temporary: true})
+                            .then(invite => {
+                                message.reply("Here's an invite code: http://discord.gg/" + invite.code)
+                            })
+                        } else {
+                            channel.createInvite(
+                                {maxUses: 1, maxAge: 0, guild: server, temporary: false})
+                                .then(invite => {
+                                    message.reply("Here's an invite code: http://discord.gg/" + invite.code)
+                                })
+                        }})
+                        .catch(() => {
+                            // handles case where user doesn't react within 30 seconds
+                            message.reply("Invite cancelled; you didn't specify a time")
+                            .then(msg => {
+                                setTimeout(() => {
+                                    msg.delete()
+                                    message.delete()
+                                }, 10000) 
+                        })})
+            })
+}
+
+function createRole(message) {
+    let roleName = ""
+    let roleEmoji = ""
+    message.reply("Enter a name for this role")
+        .then(function() {
+            message.channel.awaitMessages(response => message.content, {
+                max: 1,
+                time: 600000,
+                errors: ['time'],
+            })
+            .then(collected => {
+                // save the role name
+                roleName = collected
+                message.reply("Now react to this message with the associated emoji")
+                .then(msg => {
+                    const filter = (reaction, user) => {
+                        return user.id === message.author.id;
+                    }
+                    msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+                        .then(collected => {
+                            roleEmoji = collected.first()
+                            // TODO 1: debug the role creation
+                            // TODO 2: add new role to emojis.json
+                            //         via roleEmoji: new role fetched id
+                            // TODO 3: edit other roles to use emojis.json instead
+                            msg.guild.roles.create({
+                                data: {
+                                    name: roleName
+                                }
+                            })
+                    console.log(roleName + roleEmoji + " created")            
+                })
+                })
+                })
+            })
+}
+
+
+bot.on('ready', onReady);
+bot.on('message', onMessage);
+bot.login();
