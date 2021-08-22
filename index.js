330ce454e4291b924c4b9a3da33aef6bb864ce03
audribean <audri.saulters@gmail.com>
2021-08-22 00:46:03 -0700

    Uploading all

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const Discord = require("discord.js");
 require("dotenv-flow").config();
 const bot = new Discord.Client();
+
+const fs = require('fs');
+//const allRoles = readFromFile
+
 const prefix = "!";
+
+// TODO LIST:
+// fix the env issue
+// find json solution for storing roles
+
+// put these in an env
 const chanID = "784928448811892777";
 const guildID = "784928448367427615";
+//const chanID = process.env.GENERAL_CHANNEL_ID
+//const guildID = process.env.GUILD_ID
+//console.log("Chan ID: " + chanID + "\nGuild ID: " + guildID)
+//console.log(process.env)
 const roles = {
     '🐷': "787212472666685460",
     '🐸': "787212690388418570"
 }
+//const roles = readFromFile()
 
-// EFFECTS: outputs that the bot is logged in and pins the instruction message to the general channel
+
+// outputs that the bot is logged in and pins the instruction message to the general channel
 function onReady(){
     console.log('Logged in as ' + bot.user.tag)
 
@@ -28,9 +44,10 @@ function onReady(){
     channel.send(embed)
         .then(msg => msg.pin())
         .catch(console.error)
+    readFromFile
 }
 
-// EFFECTS: handles calls to !pin, !clean, and does nothing otherwise
+// handles calls to !pin, !clean, and does nothing otherwise
 function onMessage(message) {
     if (message.author.bot || message.author == bot.user) {
         //console.log('Ignored bot message')
@@ -52,9 +69,10 @@ function onMessage(message) {
 }
 
 // REQUIRES: (string) message to be written after the !pin
-// EFFECTS: sends a message with desired content and pins it, deleting the user's !pin call
+// sends a message with desired content and pins it, deleting the user's !pin call
 function pinIt(message) {
-    message.channel.send("@" + message.author.username + " wrote:\n " + message.content.substr(5))
+    //message.channel.send("@" + message.author.username + " wrote:\n " + message.content.substr(5))
+    message.channel.send(message.content.substr(4))
         .then(msg => msg.pin())
         .then(console.log)
         .catch(console.error)
@@ -62,7 +80,7 @@ function pinIt(message) {
 }
 
 // REQUIRES: an integer i between 1-98 inclusive to follow !clean
-// EFFECTS: if user confirms they want to clean the channel, i messages are deleted as well as the last "!clean" call
+//          if user confirms they want to clean the channel, i messages are deleted as well as the last "!clean" call
 //          if user cancels, message is sent reaffirming cancellation
 //          if user does nothing after 30 seconds, message is sent reaffirming that the user did nothing,
 //          and this message is deleted after an additional 10 seconds 
@@ -162,8 +180,8 @@ function roleReact(message) {
 }
 
 // REQUIRES: non-null string to follow !nick
-// EFFECTS: replaces the user's nickname with desired name
-//          if the user is not the owner/higher in hierarchy
+// replaces the user's nickname with desired name
+//          ONLY IF the user is not the owner/higher in hierarchy than the bot
 function newNick(message) {
     let newName = message.content.substr(6).toString()
     message.member.setNickname(newName)
@@ -172,7 +190,7 @@ function newNick(message) {
     message.reply("love the new look, " + message.member.nickname)
 }
 
-// EFFECTS: Creates a temporary invite when reacted with '😄', and a permanent invite with '🤩'
+// Creates a temporary invite when reacted with '😄', and a permanent invite with '🤩'
 function createInvite(message) {
     const server = bot.guilds.fetch(guildID)
     const channel = bot.channels.cache.get(chanID)
@@ -214,44 +232,99 @@ function createInvite(message) {
             })
 }
 
+// asks the user for the name of the role and its associated emoji,
+//          creates the role with that info, and saves the new role in the emojis.json file
 function createRole(message) {
-    let roleName = ""
-    let roleEmoji = ""
     message.reply("Enter a name for this role")
         .then(function() {
-            message.channel.awaitMessages(response => message.content, {
-                max: 1,
-                time: 600000,
-                errors: ['time'],
-            })
-            .then(collected => {
+            // collect the role name
+            const messageFilter = msg => msg.author.id === message.author.id
+            const collector = message.channel.createMessageCollector(messageFilter, {max: 1, time: 60000})
+            collector.on("collect", collected => {
                 // save the role name
-                roleName = collected
+                let roleName = collected.content
                 message.reply("Now react to this message with the associated emoji")
                 .then(msg => {
-                    const filter = (reaction, user) => {
-                        return user.id === message.author.id;
+                    // filter for desired emoji
+                    const emojiFilter = (reaction, user) => {
+                        return user.id === message.author.id
                     }
-                    msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+                    msg.awaitReactions(emojiFilter, { max: 1, time: 30000, errors: ['time'] })
                         .then(collected => {
-                            roleEmoji = collected.first()
-                            // TODO 1: debug the role creation
-                            // TODO 2: add new role to emojis.json
-                            //         via roleEmoji: new role fetched id
-                            // TODO 3: edit other roles to use emojis.json instead
+                            //collect desired emoji
+                            let roleEmoji = collected.first().emoji.name
                             msg.guild.roles.create({
                                 data: {
                                     name: roleName
                                 }
                             })
-                    console.log(roleName + roleEmoji + " created")            
+                    console.log(roleName + " " + roleEmoji + " created")
+                    //push to emojis.json
+                    pushToFile(roleName, roleEmoji)        
                 })
                 })
                 })
             })
 }
 
+// function pushToFile(roleName, roleEmoji) {
+//     allRoles.set(roleName, roleEmoji)
+//     JSON.stringify(allRoles)
+
+// }
+
+function readFromFile() {
+    fs.readFile('./emojis.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("Could not read emojis.json: ", err)
+            return
+        }
+        console.log('Successfully reading emojis.json: ', jsonString) 
+        let roleMap = new Map
+        for (var value in jsonString) {  
+            roleMap.set(value,jsonString[value])  
+            }  
+        console.log(roleMap.size)
+        return roleMap
+    })
+}
+
+// async function onReaction(msgReaction) {
+//     const message = await msgReaction.message
+//     const emoji = await msgReaction.emoji.name
+//     if (emoji === '🎃') {
+//         message.delete
+//     }
+// }
+
+// function welcome(member) {
+//     console.log("Someone joined the server!")
+//     const welcomeChannel = "790460653856882709"
+//     const embed = new Discord.MessageEmbed
+//     //const newUsers = new Discord.Collection();
+//     //newUsers.set(member.id, member.user)
+//     const name = member.displayName
+//     console.log(name)
+//     embed.setDescription("Welcome, " + name + "!")
+//     embed.setColor([117,255,255])
+//     member.guild.channels.get(welcomeChannel).send(embed)
+//         .catch(console.error)
+// }
+
 
 bot.on('ready', onReady);
 bot.on('message', onMessage);
+//bot.on("messageReactionAdd", onReaction)
+bot.on('presenceUpdate', function(oldStatus, newStatus){
+    console.log("Status update observed")
+    if (oldStatus.presence.status !== newStatus.presence.status) {
+        const welcomeChannel = "790460653856882709"
+        const embed = new Discord.MessageEmbed
+        const name = newStatus.presence.member.displayName
+        console.log(name)
+        embed.setDescription("Welcome, " + name + "!")
+        embed.setColor([117,255,255])
+        member.guild.channels.get(welcomeChannel).send(embed)
+            .catch(console.error)
+    }})
 bot.login();
